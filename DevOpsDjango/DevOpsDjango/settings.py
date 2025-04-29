9af4c1410f93cb65135e7643704ec821bbaed46b
excommunicades <daniilworkmail49@gmail.com>
2025-04-29 17:28:11 +0300

    Check commit.

--- DevOpsDjango/DevOpsDjango/settings.py
+++ DevOpsDjango/DevOpsDjango/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "DevOpsDjango.wsgi.application"
 #     "default": {
 #         "ENGINE": "django.db.backends.sqlite3",
 #         "NAME": BASE_DIR / "db.sqlite3",
-#     }
+#     } 
 # }
 DATABASES = {
     'default': {
